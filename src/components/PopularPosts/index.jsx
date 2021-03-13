55fa0b9352fcc8daf52aefed2d2ed4b0dc182a71
Yunseo Hwang <yunseo.dev@gmail.com>
2021-03-13 11:08:35 +0900

    Use Rect3dSlider component
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/components/PopularPosts/index.jsx
+++ src/components/PopularPosts/index.jsx
@@ -1,24 +1,9 @@
-import React, { useState } from 'react';
-import Rect from 'react-3d-rect';
+import React from 'react';
+import HomeElement from 'components/HomeElement';
 import PostCard from 'components/PostCard';
-import S from './style';
+import Rect3dSlider from 'components/Rect3dSlider';
 
 const PopularPosts = () => {
-  const [currentFace, setCurrentFace] = useState(0);
-  const faces = ['face', 'right', 'back', 'left'];
-
-  const nextSlide = () => {
-    setCurrentFace((currentFace + 1) % 4);
-  };
-
-  const prevSlide = () => {
-    setCurrentFace(currentFace - 1 >= 0 ? currentFace - 1 : 3);
-  };
-
-  const setSlideIndex = num => () => {
-    setCurrentFace(num % 4);
-  };
-
   const author = {
     name: 'Yunseo Hwang',
     level: 'Level 1',
@@ -34,50 +19,37 @@ const PopularPosts = () => {
   );
 
   return (
-    <S.Wrap>
-      <S.TitleWrap>
-        <S.Title>Popular Post</S.Title>
-        <span>|</span>
-        <S.SubTitle>You are gonna love this.</S.SubTitle>
-      </S.TitleWrap>
+    <HomeElement
+      title="Popular Post"
+      subtitle="You are gonna love this"
+      width="100%"
+    >
+      <Rect3dSlider size={810} height={330} useDots>
+        <>
+          {item}
+          {item}
+          {item}
+        </>
 
-      <S.PostsWrap>
-        <Rect index={faces[currentFace]} size={810}>
-          <>
-            {item}
-            {item}
-            {item}
-          </>
+        <>
+          {item}
+          {item}
+          {item}
+        </>
 
-          <>
-            {item}
-            {item}
-            {item}
-          </>
+        <>
+          {item}
+          {item}
+          {item}
+        </>
 
-          <>
-            {item}
-            {item}
-            {item}
-          </>
-
-          <>
-            {item}
-            {item}
-            {item}
-          </>
-        </Rect>
-        <S.PrevButton onClick={prevSlide}>&#10094;</S.PrevButton>
-        <S.NextButton onClick={nextSlide}>&#10095;</S.NextButton>
-      </S.PostsWrap>
-
-      <S.DotsWrap>
-        <S.Dot active={currentFace === 0} onClick={setSlideIndex(0)} />
-        <S.Dot active={currentFace === 1} onClick={setSlideIndex(1)} />
-        <S.Dot active={currentFace === 2} onClick={setSlideIndex(2)} />
-        <S.Dot active={currentFace === 3} onClick={setSlideIndex(3)} />
-      </S.DotsWrap>
-    </S.Wrap>
+        <>
+          {item}
+          {item}
+          {item}
+        </>
+      </Rect3dSlider>
+    </HomeElement>
   );
 };
 
