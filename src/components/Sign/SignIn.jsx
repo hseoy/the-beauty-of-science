e459bdee2781aa84056809ed3b023b2dbab92635
Yunseo Hwang <yunseo.dev@gmail.com>
2021-03-29 23:10:06 +0900

    Add state to SignIn and SignUp component
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/components/Sign/SignIn.jsx
+++ src/components/Sign/SignIn.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 import Input from './Input';
 import S from './style';
 
 const SignIn = ({ title }) => {
+  const [email, setEmail] = useState('hello');
+  const [password, setPassword] = useState('');
+
+  const onSubmitSignIn = () => {
+    console.log(email, password);
+  };
+
   return (
     <S.Wrap>
       <S.ContentWrap>
         <S.Title>{title}</S.Title>
         <S.Body>
-          <Input title="ID" type="text" placeholder="Enter your id..." />
+          <Input
+            title="EMAIL"
+            type="email"
+            placeholder="Enter your email..."
+            valueChangeHandler={setEmail}
+          />
           <Input
             title="PW"
             type="password"
             placeholder="Enter your password..."
+            valueChangeHandler={setPassword}
           />
-          <Button>Sign In</Button>
+          <Button clickHandler={onSubmitSignIn}>Sign In</Button>
         </S.Body>
         <S.Footer>
           <S.FooterLink to="/signup">Sign Up</S.FooterLink>
--- src/components/Sign/SignUp.jsx
+++ src/components/Sign/SignUp.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 import Input from './Input';
 import S from './style';
 
 const SignUp = ({ title }) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repassword, setRepassword] = useState('');
+
+  const onSubmitSignIn = () => {
+    console.log(username, email, password, repassword);
+  };
+
   return (
     <S.Wrap>
       <S.ContentWrap>
         <S.Title>{title}</S.Title>
         <S.Body>
-          <Input title="NAME" type="text" placeholder="Enter your name..." />
-          <Input title="ID" type="text" placeholder="Enter your id..." />
+          <Input
+            title="NAME"
+            type="text"
+            placeholder="Enter your name..."
+            valueChangeHandler={setUsername}
+          />
+          <Input
+            title="EMAIL"
+            type="email"
+            placeholder="Enter your email..."
+            valueChangeHandler={setEmail}
+          />
           <Input
             title="PW"
             type="password"
             placeholder="Enter your password..."
+            valueChangeHandler={setPassword}
           />
           <Input
             title="Re-PW"
             type="password"
             placeholder="Confirm your password..."
+            valueChangeHandler={setRepassword}
           />
-          <Button>Sign Up</Button>
+          <Button clickHandler={onSubmitSignIn}>Sign Up</Button>
         </S.Body>
         <S.Footer>
           <S.FooterLink to="/signin">Sign In</S.FooterLink>
