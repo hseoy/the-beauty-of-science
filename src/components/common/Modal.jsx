0bf81a1963209aebf05f6e7bb3dba3565ae59c4f
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-13 11:59:40 +0900

    Add Modal components
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,174 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import zIndexes from 'lib/styles/zIndexes';
+import Overlay from './Overlay';
+import Portal from './Portal';
+import ModalTemplate from './ModalTemplate';
+
+const Modal = ({
+  children,
+  closeHandler,
+  maskClosable,
+  closable,
+  visible,
+  useTemplate,
+  title,
+  button,
+  checkHandler,
+  customInner,
+  bgColor,
+  bdColor,
+  color,
+}) => {
+  useEffect(() => {
+    if (visible) {
+      document.body.style.cssText = `position:fixed; top:-${window.scrollY}px;left:-${window.scrollX}px;overflow: hidden`;
+      return () => {
+        const scrollY = document.body.style.top;
+        const scrollX = document.body.style.left;
+        document.body.style.cssText = `position: ""; top: "";left: "";`;
+        window.scrollTo(
+          parseInt(scrollX || '0', 10) * -1,
+          parseInt(scrollY || '0', 10) * -1,
+        );
+      };
+    }
+    return () => {};
+  }, [visible]);
+
+  const close = e => {
+    if (closeHandler) {
+      closeHandler(e);
+    }
+  };
+
+  const onMaskClick = e => {
+    if (e.target === e.currentTarget) {
+      close(e);
+    }
+  };
+
+  return (
+    <Portal elementId="modal-root">
+      <Overlay visible={visible} animation>
+        <ModalBlock
+          tapIndex="-1"
+          onClick={maskClosable ? onMaskClick : null}
+          $visible={visible}
+          $color={color}
+          $bdColor={bdColor}
+          $bgColor={bgColor}
+        >
+          {customInner ? (
+            children
+          ) : (
+            <div className="inner">
+              {closable && (
+                <button className="close-button" type="button" onClick={close}>
+                  &times;
+                </button>
+              )}
+              <div className="content">
+                {useTemplate ? (
+                  <ModalTemplate
+                    title={title}
+                    button={button}
+                    checkHandler={checkHandler}
+                    primaryColor={bgColor}
+                    accentColor={color}
+                  >
+                    {children}
+                  </ModalTemplate>
+                ) : (
+                  children
+                )}
+                {children}
+              </div>
+            </div>
+          )}
+        </ModalBlock>
+      </Overlay>
+    </Portal>
+  );
+};
+
+Modal.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  closeHandler: PropTypes.func,
+  maskClosable: PropTypes.bool,
+  closable: PropTypes.bool,
+  visible: PropTypes.bool,
+  useTemplate: PropTypes.bool,
+  title: PropTypes.string,
+  button: PropTypes.string,
+  checkHandler: PropTypes.func,
+  customInner: PropTypes.bool,
+  bgColor: PropTypes.string,
+  bdColor: PropTypes.string,
+  color: PropTypes.string,
+};
+
+Modal.defaultProps = {
+  closeHandler: null,
+  maskClosable: true,
+  closable: true,
+  visible: false,
+  useTemplate: false,
+  title: '',
+  button: '',
+  checkHandler: null,
+  customInner: false,
+  bgColor: '',
+  bdColor: '',
+  color: '',
+};
+
+const ModalBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.6s;
+  overflow: hidden;
+  width: 100%;
+  height: ${props => (props.$visible ? '100%' : '0%')};
+
+  > .inner {
+    box-sizing: border-box;
+    position: relative;
+    border-radius: 20px;
+    min-width: 500px;
+    overflow: hidden;
+
+    background-color: ${props =>
+      props.bgColor || props.theme.colors.primaryColor || '#fff'};
+    border: 5px solid
+      ${props => props.bdColor || props.theme.colors.accentColor || '#000'};
+    color: ${props => props.color || props.theme.colors.accentColor || '#000'};
+
+    > .close-button {
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      width: 40px;
+      height: 40px;
+      background-color: transparent;
+      font-size: 18px;
+      font-weight: bold;
+      cursor: pointer;
+      z-index: ${zIndexes.ModalCloseButton};
+    }
+
+    > .content {
+      position: relative;
+      width: 100%;
+      padding: 20px;
+      overflow-y: auto;
+    }
+  }
+`;
+
+export default Modal;
---
+++ src/components/common/ModalTemplate.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ModalTemplate = ({
+  children,
+  title,
+  button,
+  checkHandler,
+  primaryColor,
+  accentColor,
+}) => {
+  const buttonClickHandler = event => {
+    if (checkHandler) {
+      checkHandler(event);
+    }
+  };
+
+  return (
+    <ModalTemplateBlock $primaryColor={primaryColor} $accentColor={accentColor}>
+      <div className="title">{title}</div>
+      <div className="body">{children}</div>
+      <button
+        className="check-button"
+        type="button"
+        onClick={buttonClickHandler}
+      >
+        {button}
+      </button>
+    </ModalTemplateBlock>
+  );
+};
+
+ModalTemplate.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  button: PropTypes.string,
+  checkHandler: PropTypes.func,
+  primaryColor: PropTypes.string,
+  accentColor: PropTypes.string,
+};
+
+ModalTemplate.defaultProps = {
+  title: '',
+  children: {},
+  button: '',
+  checkHandler: undefined,
+  primaryColor: '',
+  accentColor: '',
+};
+
+const ModalTemplateBlock = styled.div`
+  > .title {
+    width: 100%;
+    height: 40%;
+    font-size: 2rem;
+    padding-top: 0.67rem;
+    padding-bottom: 0.67rem;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    word-break: break-all;
+    font-family: ${({ theme }) => theme.font.family.accent || 'sans-seif'};
+    color: ${props =>
+      props.$accentColor || props.theme.colors.accentColor || '#000'};
+    border-bottom: 2px dotted
+      ${props => props.$accentColor || props.theme.colors.accentColor || '#000'};
+  }
+
+  > .body {
+    width: 100%;
+    height: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    padding: 2rem 0;
+    color: ${props =>
+      props.$accentColor || props.theme.colors.accentColor || '#000'};
+  }
+
+  > .check-button {
+    width: 100%;
+    height: 60px;
+    border-radius: 15px;
+    font-size: 20px;
+    font-weight: bold;
+    font-family: ${({ theme }) => theme.font.family.accent || 'sans-seif'};
+    color: ${props =>
+      props.$primaryColor || props.theme.colors.primaryColor || '#fff'};
+    background-color: ${props =>
+      props.$accentColor || props.theme.colors.accentColor || '#000'};
+  }
+`;
+
+export default ModalTemplate;
