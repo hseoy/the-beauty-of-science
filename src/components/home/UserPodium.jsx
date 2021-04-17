c00abe2cb95c967b6fa08858e9605ac64b32f7c3
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-17 19:56:09 +0900

    Add UserPodium component
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

---
+++ src/components/home/UserPodium.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Podium from 'components/home/Podium';
+import Options from 'components/common/Options';
+import HomeElement from './HomeElement';
+
+const UserPodium = ({
+  maintitle,
+  subtitle,
+  option,
+  optionList,
+  onChangeOption,
+  users,
+}) => {
+  const bars = users.map(user => ({ top: user.title, bottom: user.name }));
+
+  while (bars.length < 3) {
+    bars.push({ top: '', bottom: '' });
+  }
+
+  return (
+    <HomeElement maintitle={maintitle} subtitle={subtitle} width="50%">
+      <UserPodiumBlock>
+        {optionList && (
+          <Options
+            option={option}
+            optionList={optionList}
+            onChange={onChangeOption}
+          />
+        )}
+        <div className="podium">
+          <Podium bars={bars} />
+        </div>
+      </UserPodiumBlock>
+    </HomeElement>
+  );
+};
+
+UserPodium.propTypes = {
+  maintitle: PropTypes.string,
+  subtitle: PropTypes.string,
+  option: PropTypes.string,
+  optionList: PropTypes.arrayOf(PropTypes.string),
+  onChangeOption: PropTypes.func,
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+UserPodium.defaultProps = {
+  maintitle: 'Top User',
+  subtitle: 'The most active users',
+  option: '',
+  optionList: [],
+  onChangeOption: null,
+  users: [],
+};
+
+const UserPodiumBlock = styled.div`
+  width: 100%;
+  height: 100%;
+
+  > .podium {
+    height: 200px;
+    width: 100%;
+    position: relative;
+  }
+`;
+
+export default UserPodium;
