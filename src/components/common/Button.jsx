a53967764a821686b205c48ce153982845701344
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-20 07:54:14 +0900

    Fix typo
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -70,7 +70,7 @@ const StyledButton = styled.button`
       ? props.theme.colors.primaryColor || '#fff'
       : props.theme.colors.accentColor || '#000'};
   border: 3px solid ${({ theme }) => theme.colors.accentColor || '#000'};
-  border-radius: ${props => props.$boarderRadius || '0'};
+  border-radius: ${props => props.$borderRadius || '0'};
   border-radius: 3px;
   font-family: ${({ theme }) => theme.font.family.accent || 'sans-seif'};
   font-size: ${props => props.$fontSize || '1rem'};
