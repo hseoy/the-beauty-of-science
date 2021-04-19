f2473c51c7eff47a2c02e1348850a743381db75a
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-20 07:42:48 +0900

    Add props of Button component to LinkButton component
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/components/common/LinkButton.jsx
+++ src/components/common/LinkButton.jsx
@@ -3,17 +3,56 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 
-const LinkButton = ({ transition, to, ...rest }) => (
-  <Button transition={transition} as={Link} to={to} {...rest} />
+const LinkButton = ({
+  to,
+  transition,
+  fontSize,
+  borderRadius,
+  fullwidth,
+  reverse,
+  hover,
+  padding,
+  children,
+  ...rest
+}) => (
+  <Button
+    as={Link}
+    to={to}
+    transition={transition}
+    fontSize={fontSize}
+    borderRadius={borderRadius}
+    fullwidth={fullwidth}
+    reverse={reverse}
+    hover={hover}
+    padding={padding}
+    {...rest}
+  />
 );
 
 LinkButton.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
   transition: PropTypes.string,
+  fontSize: PropTypes.string,
+  borderRadius: PropTypes.string,
+  padding: PropTypes.string,
+  fullwidth: PropTypes.bool,
+  reverse: PropTypes.bool,
+  hover: PropTypes.bool,
   to: PropTypes.string.isRequired,
 };
 
 LinkButton.defaultProps = {
+  children: null,
   transition: '',
+  fontSize: '',
+  borderRadius: '',
+  padding: '',
+  fullwidth: false,
+  reverse: false,
+  hover: true,
 };
 
 export default LinkButton;
