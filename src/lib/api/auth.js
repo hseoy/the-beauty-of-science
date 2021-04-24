52696142ded090c8f2b4501335dc8150f99fa71e
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-24 17:41:53 +0900

    Set access token to header
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,10 +1,20 @@
 import client from './client';
 
 export const signin = ({ email, password }) =>
-  client.post('/api/auth/signin', { email, password });
+  client.post('/api/auth/signin', { email, password }).then(response => {
+    const { access } = response.data;
+    client.defaults.headers.common.Authorization = `Bearer ${access}`;
+    return response;
+  });
 
 export const signup = ({ email, username, password }) =>
-  client.post('/api/auth/signup', { email, username, password });
+  client
+    .post('/api/auth/signup', { email, username, password })
+    .then(response => {
+      const { access } = response.data;
+      client.defaults.headers.common.Authorization = `Bearer ${access}`;
+      return response;
+    });
 
 export const signout = () => client.get('/api/auth/signout');
 
