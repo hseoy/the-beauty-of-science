d8644926db4c295f25fcf1a826a14123b5674cd7
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-26 23:31:36 +0900

    Add getMeAvatarFile api function
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/lib/api/user.js
+++ src/lib/api/user.js
@@ -1,4 +1,19 @@
 import client from './client';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getMe = () => client.get('/api/users/me');
+
+export const getMeAvatarFile = async () => {
+  const response = await client.get('/api/users/me/avatar', {
+    responseType: 'arraybuffer',
+  });
+  const blob = new Blob([response.data], {
+    type: response.headers['content-type'],
+  });
+  const image = await new Promise(resolve => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+  response.data = image;
+  return response;
+};
