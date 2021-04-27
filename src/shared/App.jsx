e94b15d3fccafa0462f7464459390ea951667518
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-27 17:21:05 +0900

    Add init loading animation
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import HomePage from 'pages/HomePage';
 import SignupPage from 'pages/SignupPage';
@@ -6,13 +6,26 @@ import SigninPage from 'pages/SigninPage';
 import PublicRoute from 'components/route/PublicRoute';
 import { refresh } from 'modules/auth';
 import { setUser, setAvatar } from 'modules/user';
+import Loader from 'components/common/Loader';
 // import PrivateRoute from 'components/route/PrivateRoute';
 
 const App = () => {
+  const [initLoaded, setInitLoaded] = useState(false);
   const dispatch = useDispatch();
-  const { auth, refreshError } = useSelector(state => ({
+  const {
+    auth,
+    refreshError,
+    user,
+    avatar,
+    setUserError,
+    setAvatarError,
+  } = useSelector(state => ({
     auth: state.auth.auth,
     refreshError: state.auth.refreshError,
+    user: state.user.user,
+    avatar: state.user.avatar,
+    setUserError: state.user.setUserError,
+    setAvatarError: state.user.setAvatarError,
   }));
 
   useEffect(() => {
@@ -25,15 +38,24 @@ const App = () => {
       dispatch(setUser());
     }
     if (refreshError) {
-      console.log('ERROR');
+      setInitLoaded(true);
     }
   }, [auth, refreshError]);
 
+  useEffect(() => {
+    if (user || setUserError || avatar || setAvatarError) {
+      setInitLoaded(true);
+    }
+  }, [user, avatar, setUserError, setAvatarError]);
+
   return (
     <>
-      <PublicRoute exact path="/" component={HomePage} />
-      <PublicRoute restricted exact path="/signin" component={SigninPage} />
-      <PublicRoute restricted exact path="/signup" component={SignupPage} />
+      <>
+        <PublicRoute exact path="/" component={HomePage} />
+        <PublicRoute restricted exact path="/signin" component={SigninPage} />
+        <PublicRoute restricted exact path="/signup" component={SignupPage} />
+      </>
+      {!initLoaded && <Loader />}
     </>
   );
 };
